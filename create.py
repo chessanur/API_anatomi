1074ffcf4dae72698f56b6cf38e492cac7cf75c3
chessa <chessanur@gmail.com>
2022-02-27 12:56:13 +0700

    first commit

---
+++ create.py
@@ -0,0 +1,28 @@
+''' Import library '''
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
+''' Class Item Model '''
+class Item(BaseModel):
+    pretest: int
+    anatomi_telinga: int
+    posttest: int
+
+
+# Inisiasi router
+app = FastAPI()
+
+
+@app.post('/progress')
+async def create(item: Item):
+    # Mendapatkan input data
+    pretest = item.pretest
+    anatomi_telinga = item.anatomi_telinga
+    posttest = item.posttest
+
+    return item
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
